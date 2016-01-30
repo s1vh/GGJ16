8a7e85f45b1fc7b0306f5b53e377fcf4c658c9ea
jramongil <javier.ramongil@gmail.com>
2016-01-30 23:00:27 +0100

    check sequence dish

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -10,6 +10,8 @@ public class Level1 : MonoBehaviour {
 
 	public List<int> seq2 = new List<int>();
 
+	bool show = true;
+
 
 	// Use this for initialization
 	void Start () {
@@ -29,7 +31,15 @@ public class Level1 : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (dish);
+		//Debug.Log (dish);
+		if(seq2.Count == 4 && show == true){
+			showSeqUser ();
+			compareSeqs ();
+			seq2.Clear();
+		}
+			
+
+
 	}
 
 	private void createSeq(int r){
@@ -41,6 +51,28 @@ public class Level1 : MonoBehaviour {
 	
 		seq.Add (n);
 
+	}
+
+	private void showSeqUser(){
+
+		for (int i = 0; i < seq2.Count; i++) {
+			Debug.Log (seq2 [i]);
+		}
+			
+		show = false;
+
+	}
+
+	private void compareSeqs(){
+
+		if (seq2.Count == 4 && seq [0] == seq2 [0] && seq [1] == seq2 [1] && seq [2] == seq2 [2] && seq [3] == seq2 [3]) {
+			win ();
+		}
+
+	}
+
+	private void win(){
+		Debug.Log("WIN");
 	}
 		
 }
