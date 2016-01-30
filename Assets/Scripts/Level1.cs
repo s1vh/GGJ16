547d2484c9f418ecf55337ba604abaec8ba28d0f
jramongil <javier.ramongil@gmail.com>
2016-01-30 18:17:26 +0100

    Level1

---
+++ Assets/Scripts/Level1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Level1 : MonoBehaviour {
+
+	public List<int> seq = new List<int>();
+	public int dish;
+
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i <= 3; i++){
+			createSeq (4);
+
+		}
+
+		for (int i = 0; i < seq.Count; i++){
+			Debug.Log (seq[i]);
+		}
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (dish);
+	}
+
+	private void createSeq(int r){
+
+		int n;
+		do {
+			n = Random.Range (1, r + 1);
+		} while (seq.Contains (n));
+	
+		seq.Add (n);
+
+	}
+		
+}
---
+++ Assets/dish1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dish1 : MonoBehaviour {
+
+	Level1 level1;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+
+		Level1 dishLevel1 = gameObject.GetComponent<Level1> ();
+		dishLevel1.dish = 1;
+
+
+	}
+}
