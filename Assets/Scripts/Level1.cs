9ded543e86be09f3d70bf3adb33f3aba4949e584
Miguel Campins <al285854@uji.es>
2016-02-01 12:37:24 +0100

    control point

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -3,7 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Level1 : MonoBehaviour {
+public class Level1 : MonoBehaviour
+{
+    public GameObject backgroundLevel1;
+    cameraScript game;
 
     public List<int> seq = new List<int>();
     public int nOpt = 4;
@@ -13,32 +16,30 @@ public class Level1 : MonoBehaviour {
     public bool locked = true;
 
 	// Use this for initialization
-	void Start () {
-
-        buildSeq(nOpt);
-
-	}
+	void Start ()
+    {
+        game = backgroundLevel1.GetComponent<cameraScript>();
+    }
 
 	// Update is called once per frame
-	void Update () {
-			
+	void Update ()
+    {
+
 	}
 
-    public void buildSeq(int r) {   // r --> nOpt
-
+    public void buildSeq(int r)     // r --> nOpt
+    {   
         locked = true;
 
-        while (seq.Count > 0) {
-
+        while (seq.Count > 0)
+        {
             seq.RemoveAt(0);        // clear the old Array if there is one
-
         }
 
-        for (int i = 0; i < nSeq + difficulty; i++) {
-
+        for (int i = 0; i < nSeq + difficulty; i++)
+        {
             int n = Random.Range(1, r + 1);
             seq.Add(n);
-
         }
 
         showSeq(seq);
@@ -46,12 +47,11 @@ public class Level1 : MonoBehaviour {
 
     }
 
-    public void showSeq(List<int> l) {
-
-        for (int i = 0; i < l.Count; i++) {    // trace
-
+    public void showSeq(List<int> l)
+    {
+        for (int i = 0; i < l.Count; i++)   // trace
+        {    
             Debug.Log(l[i]);
-
         }
 
     }
